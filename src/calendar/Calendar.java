385277c55e27524c60065e781174dd0e50f2f811
JaneDoe <JaneDoe@JaneDoe-PC>
2020-02-11 18:45:12 +0900

    Update get Max days of month using while

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -21,25 +21,25 @@ public class Calendar {
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수 출력하는 프로그램
+		
+		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("반복 횟수를 입력하세요");
-		int roof = scanner.nextInt();
-		
-		System.out.println();
-		
-		System.out.println("월을 입력하세요.");
-		int[] months = new int[roof];
-		for (int i = 0; i < months.length; i++) {
-			months[i] = scanner.nextInt();
-		}
 
-		System.out.println();
-		
-		for(int i=0; i<months.length; i++) {
-			System.out.printf("%d월은 %d일까지 있습니다.\n", months[i], cal.getMaxDaysOfMonth(months[i]));
+		int month = 1;
+		while(true) {
+			System.out.println("월을 입력하세요");
+			System.out.print(PROMPT);
+			 month = scanner.nextInt();
+			 if(month == -1) {
+				 break;
+			 }
+			 if(month > 12) {
+				 continue;
+			 }
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
-
+		System.out.println("Bye~");
 		scanner.close();
 
 	}
