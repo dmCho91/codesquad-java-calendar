678e647901713e785d9c3c486ba6b8bd29b9459f
JaneDoe <JaneDoe@JaneDoe-PC>
2020-02-11 18:36:17 +0900

    Add repeat function

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,12 +3,12 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month-1];
+		return MAX_DAYS[month - 1];
 	}
-	
+
 	public void printSampleCalendar() {
 		System.out.println(" 일   월   화   수   목   금   토");
 		System.out.println("--------------------");
@@ -20,16 +20,28 @@ public class Calendar {
 
 	public static void main(String[] args) {
 
-		//숫자를 입력받아 해당하는 달의 최대 일수 출력하는 프로그램
+		// 숫자를 입력받아 해당하는 달의 최대 일수 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
+		System.out.println("반복 횟수를 입력하세요");
+		int roof = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		System.out.println();
 		
+		System.out.println("월을 입력하세요.");
+		int[] months = new int[roof];
+		for (int i = 0; i < months.length; i++) {
+			months[i] = scanner.nextInt();
+		}
+
+		System.out.println();
+		
+		for(int i=0; i<months.length; i++) {
+			System.out.printf("%d월은 %d일까지 있습니다.\n", months[i], cal.getMaxDaysOfMonth(months[i]));
+		}
+
 		scanner.close();
-		
+
 	}
 
 }
