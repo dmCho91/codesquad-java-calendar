334fcbc36c1f40b8bc98a505474bad7c09552560
JaneDoe <JaneDoe@JaneDoe-PC>
2020-02-13 17:11:54 +0900

    update simple ui

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,8 +1,8 @@
 package calendar;
 
 public class Calendar {
-	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	// 윤년 구하기
 	public boolean isLeapYear(int year) {
@@ -26,14 +26,14 @@ public class Calendar {
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
-		//get weekday automatically
+		// get weekday automatically
 		int weekday = getWeekday(year, month, 1);
-		
+
 		// print blank space
 		for (int i = 0; i < weekday; i++) {
 			System.out.print("   ");
 		}
-		
+
 		int maxDay = getMaxDaysOfMonth(year, month);
 		int count = 7 - weekday;
 		int delim = (count < 7) ? count : 0;
@@ -58,35 +58,25 @@ public class Calendar {
 
 	private int getWeekday(int year, int month, int day) {
 		int syear = 1970;
-		final int STANDARD_WEEKDAY = 3; // 1970/Jan/1st = Thursday
-		
+		final int STANDARD_WEEKDAY = 4; // 1970/Jan/1st = Thursday
+
 		int count = 0;
-		
-		for(int i = syear; i <year; i++) {
-			int delta = isLeapYear(i) ? 366: 365;
+
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
 			count += delta;
 		}
-		
-//		System.out.println(count);
-		
-		for(int i=1; i<month; i++) {
+
+		for (int i = 1; i < month; i++) {
 			int delta = getMaxDaysOfMonth(year, i);
 			count += delta;
 		}
-		
-		count+= day;
-		
-		
+
+		count += day -1;
+
 		int weekday = (count + STANDARD_WEEKDAY) % 7;
+
 		return weekday;
 	}
 
-	public static void main(String[] args) {
-		Calendar c = new Calendar();
-		System.out.println(c.getWeekday(1970, 1, 1) == 3);
-		System.out.println(c.getWeekday(1971, 1, 1) == 4);
-		System.out.println(c.getWeekday(1972, 1, 1) == 5);
-		System.out.println(c.getWeekday(1973, 1, 1) == 0);
-		System.out.println(c.getWeekday(1974, 1, 1) == 1);
-	}
 }
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -3,53 +3,76 @@ package calendar;
 import java.util.Scanner;
 
 public class Prompt {
+
+	public void printMenu() {
+		System.out.println("+----------------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말    q.종료");
+		System.out.println("+----------------------------+");
+	}
+
 	/**
 	 * 
 	 * @param week 요일명
 	 * @return 0~6 (0 = Sunday, 6 = Saturday)
 	 */
 	public int parseDay(String week) {
-		if(week.equals("su")) return 0;
-		else if(week.equals("mo")) return 1;
-		else if(week.equals("tu")) return 2;
-		else if(week.equals("we")) return 3;
-		else if(week.equals("th")) return 4;
-		else if(week.equals("fr")) return 5;
-		else if(week.equals("sa")) return 6;
+		if (week.equals("su")) return 0;
+		else if (week.equals("mo")) return 1;
+		else if (week.equals("tu")) return 2;
+		else if (week.equals("we")) return 3;
+		else if (week.equals("th")) return 4;
+		else if (week.equals("fr")) return 5;
+		else if (week.equals("sa")) return 6;
 		else
 			return 0;
 	}
-	
 
 	public void runPrompt() {
+		printMenu();
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
+		while (true) {
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = scanner.next();
+			if (cmd.contentEquals("1")) cmdRegister();
+			else if (cmd.contentEquals("2")) cmdSearch();
+			else if (cmd.contentEquals("3")) cmdCal(scanner, cal);
+			else if (cmd.contentEquals("h")) printMenu();
+			else if (cmd.contentEquals("q")) break;
+		}
+		System.out.println("Thank you, Bye~");
+
+		scanner.close();
+	}
+
+	private void cmdCal(Scanner s, Calendar c) {
 		int month = 1;
 		int year = 2017;
 
-		while (true) {
-			System.out.println("년도를 입력하세요 (exit: -1)");
-			System.out.print("YEAR >");
-			year = scanner.nextInt();
-			if(year == -1)
-				break;
-			
-			System.out.println("월을 입력하세요");
-			System.out.print("MONTH >");
-			month = scanner.nextInt();
-			
-			if (month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.\n");
-				continue;
-			}
-			
-			
-			cal.printCalendar(year, month);
+		System.out.println("년도를 입력하세요 (exit: -1)");
+		System.out.print("YEAR >");
+		year = s.nextInt();
+
+		System.out.println("월을 입력하세요");
+		System.out.print("MONTH >");
+		month = s.nextInt();
+
+		if (month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.\n");
+			return;
 		}
-		System.out.println("Bye~");
-		
-		scanner.close();
+
+		c.printCalendar(year, month);
+	}
+
+	private void cmdSearch() {
+	}
+
+	private void cmdRegister() {
 	}
 
 	public static void main(String[] args) {
